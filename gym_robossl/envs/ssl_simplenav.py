6803e9da8be77bba05ccbe79380ef6c5fd36b9ce
Cheng Xie <cheng.k.xie@gmail.com>
2016-11-19 15:48:08 -0800

    Adding robots

--- gym_robossl/envs/ssl_simplenav.py
+++ gym_robossl/envs/ssl_simplenav.py
@@ -31,6 +31,7 @@ SIDE_ENGINE_AWAY   = 12.0
 VIEWPORT_W = 600
 VIEWPORT_H = 400
 
+'''
 class ContactDetector(contactListener):
     def __init__(self, env):
         contactListener.__init__(self)
@@ -45,6 +46,7 @@ class ContactDetector(contactListener):
         for i in range(2):
             if self.env.legs[i] in [contact.fixtureA.body, contact.fixtureB.body]:
                 self.env.legs[i].ground_contact = False
+'''
 
 class SSLSimpleNav(gym.Env):
     metadata = {
@@ -84,18 +86,18 @@ class SSLSimpleNav(gym.Env):
 
     def _destroy(self):
         if not self.moon: return
-        self.world.contactListener = None
+        #self.world.contactListener = None
         self.world.DestroyBody(self.moon)
         self.moon = None
-        self.world.DestroyBody(self.lander)
-        self.lander = None
-        self.world.DestroyBody(self.legs[0])
-        self.world.DestroyBody(self.legs[1])
+        self.world.DestroyBody(self.robot)
+        self.robot = None
+        #self.world.DestroyBody(self.legs[0])
+        #self.world.DestroyBody(self.legs[1])
 
     def _reset(self):
         self._destroy()
-        self.world.contactListener_keepref = ContactDetector(self)
-        self.world.contactListener = self.world.contactListener_keepref
+        #self.world.contactListener_keepref = ContactDetector(self)
+        #self.world.contactListener = self.world.contactListener_keepref
         self.game_over = False
         self.prev_shaping = None
 
@@ -131,71 +133,34 @@ class SSLSimpleNav(gym.Env):
         self.moon.color2 = (0.0,0.0,0.0)
 
         initial_y = VIEWPORT_H/SCALE
-        self.lander = self.world.CreateDynamicBody(
+        self.robot = self.world.CreateDynamicBody(
             position = (VIEWPORT_W/SCALE/2, initial_y),
             angle=0.0,
             fixtures = fixtureDef(
-                shape=polygonShape(vertices=[ (x/SCALE,y/SCALE) for x,y in LANDER_POLY ]),
+                shape=circleShape(radius=10/SCALE, pos=(0,0)),
                 density=5.0,
                 friction=0.1,
                 categoryBits=0x0010,
                 maskBits=0x001,  # collide only with ground
-                restitution=0.0) # 0.99 bouncy
+                restitution=0.5) # 0.99 bouncy
                 )
-        self.lander.color1 = (0.5,0.4,0.9)
-        self.lander.color2 = (0.3,0.3,0.5)
-        self.lander.ApplyForceToCenter( (
+        self.robot.color1 = (0.5,0.4,0.9)
+        self.robot.color2 = (0.5,0.4,0.9)
+
+        self.robot.ApplyForceToCenter( (
             self.np_random.uniform(-INITIAL_RANDOM, INITIAL_RANDOM),
             self.np_random.uniform(-INITIAL_RANDOM, INITIAL_RANDOM)
             ), True)
 
-        self.legs = []
-        for i in [-1,+1]:
-            leg = self.world.CreateDynamicBody(
-                position = (VIEWPORT_W/SCALE/2 - i*LEG_AWAY/SCALE, initial_y),
-                angle = (i*0.05),
-                fixtures = fixtureDef(
-                    shape=polygonShape(box=(LEG_W/SCALE, LEG_H/SCALE)),
-                    density=1.0,
-                    restitution=0.0,
-                    categoryBits=0x0020,
-                    maskBits=0x001)
-                )
-            leg.ground_contact = False
-            leg.color1 = (0.5,0.4,0.9)
-            leg.color2 = (0.3,0.3,0.5)
-            rjd = revoluteJointDef(
-                bodyA=self.lander,
-                bodyB=leg,
-                localAnchorA=(0, 0),
-                localAnchorB=(i*LEG_AWAY/SCALE, LEG_DOWN/SCALE),
-                enableMotor=True,
-                enableLimit=True,
-                maxMotorTorque=LEG_SPRING_TORQUE,
-                motorSpeed=+0.3*i  # low enough not to jump back into the sky
-                )
-            if i==-1:
-                rjd.lowerAngle = +0.9 - 0.5  # Yes, the most esoteric numbers here, angles legs have freedom to travel within
-                rjd.upperAngle = +0.9
-            else:
-                rjd.lowerAngle = -0.9
-                rjd.upperAngle = -0.9 + 0.5
-            leg.joint = self.world.CreateJoint(rjd)
-            self.legs.append(leg)
-
-        self.drawlist = [self.lander] + self.legs
+        self.drawlist = [self.robot]
 
         return self._step(np.array([0,0]) if self.continuous else 0)[0]
     
     def _step(self, action):
         assert self.action_space.contains(action), "%r (%s) invalid " % (action,type(action))
-
-        # Engines
-        tip  = (math.sin(self.lander.angle), math.cos(self.lander.angle))
-        side = (-tip[1], tip[0]);
-        dispersion = [self.np_random.uniform(-1.0, +1.0) / SCALE for _ in range(2)]
-
+        
         m_power = 0.0
+        '''
         if (self.continuous and action[0] > 0.0) or (not self.continuous and action==2):
             # Main engine
             if self.continuous:
@@ -207,8 +172,9 @@ class SSLSimpleNav(gym.Env):
             oy = -tip[1]*(4/SCALE + 2*dispersion[0]) - side[1]*dispersion[1]
             impulse_pos = (self.lander.position[0] + ox, self.lander.position[1] + oy)
             self.lander.ApplyLinearImpulse( (-ox*MAIN_ENGINE_POWER*m_power, -oy*MAIN_ENGINE_POWER*m_power), impulse_pos, True)
-
+        '''
         s_power = 0.0
+        '''
         if (self.continuous and np.abs(action[1]) > 0.5) or (not self.continuous and action in [1,3]):
             # Orientation engines
             if self.continuous:
@@ -222,20 +188,21 @@ class SSLSimpleNav(gym.Env):
             oy = -tip[1]*dispersion[0] - side[1]*(3*dispersion[1]+direction*SIDE_ENGINE_AWAY/SCALE)
             impulse_pos = (self.lander.position[0] + ox - tip[0]*17/SCALE, self.lander.position[1] + oy + tip[1]*SIDE_ENGINE_HEIGHT/SCALE)
             self.lander.ApplyLinearImpulse( (-ox*SIDE_ENGINE_POWER*s_power, -oy*SIDE_ENGINE_POWER*s_power), impulse_pos, True)
+        '''
 
         self.world.Step(1.0/FPS, 6*30, 2*30)
 
-        pos = self.lander.position
-        vel = self.lander.linearVelocity
+        pos = self.robot.position
+        vel = self.robot.linearVelocity
         state = [
             (pos.x - VIEWPORT_W/SCALE/2) / (VIEWPORT_W/SCALE/2),
             (pos.y - (self.helipad_y+LEG_DOWN/SCALE)) / (VIEWPORT_W/SCALE/2),
             vel.x*(VIEWPORT_W/SCALE/2)/FPS,
             vel.y*(VIEWPORT_H/SCALE/2)/FPS,
-            self.lander.angle,
-            20.0*self.lander.angularVelocity/FPS,
-            1.0 if self.legs[0].ground_contact else 0.0,
-            1.0 if self.legs[1].ground_contact else 0.0
+            self.robot.angle,
+            20.0*self.robot.angularVelocity/FPS,
+            0.0,
+            0.0
             ]
         assert len(state)==8
 
@@ -256,7 +223,7 @@ class SSLSimpleNav(gym.Env):
         if self.game_over or abs(state[0]) >= 1.0:
             done   = True
             reward = -100
-        if not self.lander.awake:
+        if not self.robot.awake:
             done   = True
             reward = +100
         return np.array(state), reward, done, {}
--- gym_robossl/envs/utils/robot.py
+++ gym_robossl/envs/utils/robot.py
@@ -2,18 +2,27 @@ class Robot:
     self._ROBOT_RADIUS
 
     self.box2d_body = None
-    def __init__(self):
-        self.box2d_body = fixtureDef(
-                shape=circleShape(radius=self._ROBOT_RADIUS, pos=(0,0)),
-                density=mass,
-                friction=0.1,
-                categoryBits=0x0100,
-                maskBits=0x001,  # collide only with ground
-                restitution=0.3)
-                )
-    def draw():
     
-    def reset():
+    def __init__(self, world, position):
+
+
+
+    def render(self, viewer):
+        self.viewer.draw_circle(f.shape.radius, 20, color=obj.color1).add_attr(t)
+        self.viewer.draw_circle(f.shape.radius, 20, color=obj.color2, filled=False, linewidth=2).add_attr(t)
+
+    def reset(self, world, position, angle):
+        self.robot = self.world.CreateDynamicBody(
+            position = position,
+            angle = angle,
+            fixtures = fixtureDef(
+                shape=circleShape(radius=10/SCALE, pos=(0,0)),
+                density=5.0,
+                friction=0.1,
+                categoryBits=0x0010,
+                maskBits=0x001,  # collide only with ground
+                restitution=0.5) # 0.99 bouncy
+                )
 
 # We control
 class FriendlyRobot(Robot):
